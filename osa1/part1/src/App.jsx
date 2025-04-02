e2a5179383e04cddb00c124c4fdf6a8b6059de98
aleksi-to <aleksi.tolppi@proton.me>
2025-04-02 12:45:49 +0300

    1.6 toimii

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
 
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <>code here</>;
+  const increaseGood = () => setGood(good + 1);
+  const increaseNeutral = () => setNeutral(neutral + 1);
+  const increaseBad = () => setBad(bad + 1);
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button onClick={increaseGood} text={"good"} />
+      <Button onClick={increaseNeutral} text={"neutral"} />
+      <Button onClick={increaseBad} text={"bad"} />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
 };
 
 export default App;
