42560c4b38277376895ae8abdeda7213bcd97db1
aleksi-to <aleksi.tolppi@proton.me>
2025-04-02 12:10:01 +0300

    Tehtävä 1.6 alustus

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,85 +1,11 @@
-const Header = (props) => {
-  console.log(props);
-  console.log("OH NO!");
-  return (
-    <div>
-      <h1>{props.course[name]}</h1>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <Part
-        name={props.course.parts[0].name}
-        exercises={props.course.parts[0].exercises}
-      />
-      <Part
-        name={props.course.parts[1].name}
-        exercises={props.course.parts[1].exercises}
-      />
-      <Part
-        name={props.course.parts[2].name}
-        exercises={props.course.parts[2].exercises}
-      />
-    </div>
-  );
-};
-
-const Part = (props) => {
-  console.log(props);
-  return (
-    <>
-      <p>
-        {props.name} {props.exercises}
-      </p>
-    </>
-  );
-};
-
-const Total = (props) => {
-  console.log(props);
-
-  return (
-    <>
-      <p>
-        Number of exercises: +{" "}
-        {props.course.parts[0].exercises +
-          props.course.parts[1].exercises +
-          props.course.parts[2].exercises}
-      </p>
-    </>
-  );
-};
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
+  return <>code here</>;
 };
 
 export default App;
