8b1eef1082177bd614cb7871a10dfa8997cec236
aleksi-to <aleksi.tolppi@proton.me>
2025-04-11 09:15:46 +0300

    1.14 Anekdootit step 3 valmis

--- osa1/part1/Anekdootit/Anekdootit/src/App.jsx
+++ osa1/part1/Anekdootit/Anekdootit/src/App.jsx
@@ -4,6 +4,26 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+const Best = ({ anecdotes, allclicks, max }) => {
+  return (
+    <>
+      <h2>Anecdote with the most votes</h2>
+      <p>{anecdotes[max]}</p>
+      <p>has {allclicks[max]} votes</p>
+    </>
+  );
+};
+
+const Anecdote = ({ anecdotes, selected, allclicks }) => {
+  return (
+    <>
+      <h2>Anecdote of the day</h2>
+      <p>{anecdotes[selected]}</p>
+      <p>has votes {allclicks[selected]}</p>
+    </>
+  );
+};
+
 const App = () => {
   const anecdotes = [
     "If it hurts, do it more often.",
@@ -18,24 +38,40 @@ const App = () => {
 
   const [selected, setSelected] = useState(0);
   const [allclicks, setClicks] = useState(new Uint8Array(anecdotes.length));
+  const [max, setMax] = useState(0);
 
-  const handleClick = () => {
+  const handleNext = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomIndex);
+    handleMax();
   };
 
   const handleVote = () => {
     const copy = [...allclicks];
     copy[selected] += 1;
     setClicks(copy);
+    handleMax();
+  };
+
+  const handleMax = () => {
+    for (let i = 0; i < allclicks.length; i++) {
+      if (allclicks[i] > allclicks[max]) {
+        setMax(i);
+      }
+    }
   };
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has votes {allclicks[selected]}</p>
+      <Anecdote
+        anecdotes={anecdotes}
+        selected={selected}
+        allclicks={allclicks}
+      />
       <Button onClick={handleVote} text={"vote"} />
-      <Button onClick={handleClick} text={"next anecdote"} />
+      <Button onClick={handleNext} text={"next anecdote"} />
+      <Button onClick={handleMax} text={"MAX"} />
+      <Best anecdotes={anecdotes} allclicks={allclicks} max={max} />
     </div>
   );
 };
